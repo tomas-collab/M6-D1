81050dbe9ce54951374111c019109ab365f20160
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-08-23 15:06:52 +0200

    inserted 5 agatha books

---
+++ insert-5-agatha-christie-books.sql
@@ -0,0 +1,5 @@
+INSERT INTO  books(name,category,cover,author) VALUES('Five Little Pigs','Adventures','FiveLittlePigs.jpg','Agatta Christie');
+ INSERT INTO  books(name,category,cover,author) VALUES('Crooked House','Adventures','CrookedHouse.jpg','Agatta Christie');
+ INSERT INTO  books(name,category,cover,author) VALUES('Endless Night','Adventures','EndlessNight.jpg','Agatta Christie');
+ INSERT INTO  books(name,category,cover,author) VALUES('The ABC Murders','Adventures','TheABCMurders.jpg','Agatta Christie');
+ INSERT INTO  books(name,category,cover,author) VALUES('Curtain','Adventures','Curtain.jpg','Agatta Christie');
