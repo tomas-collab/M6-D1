c9b6c02bf0d6232109fae15cd396902091a3f303
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-08-23 14:36:01 +0200

    authors inserted

---
+++ insert-authors.sql
@@ -0,0 +1,8 @@
+ INSERT INTO  authors(name,last_name, country,birth_year) VALUES('Dan','Brown','Spain',90);
+ INSERT INTO  authors(name,last_name, country,birth_year) VALUES('Agatha','Christie','Argentina',78);
+ INSERT INTO  authors(name,last_name, country,birth_year) VALUES('Paulo','Coelho','Germany',34);
+ INSERT INTO  authors(name,last_name, country,birth_year) VALUES('Ken','Follett','SouthAfrica',69);
+ INSERT INTO  authors(name,last_name, country,birth_year) VALUES('John','Grisham','Sweden',56);
+ INSERT INTO  authors(name,last_name, country,birth_year) VALUES('Stephen','King','Australia',44);
+ INSERT INTO  authors(name,last_name, country,birth_year) VALUES('Haruki','Murakami','Japan',67);
+ INSERT INTO  authors(name,last_name, country,birth_year) VALUES('James','Patterson','Uk',56);
