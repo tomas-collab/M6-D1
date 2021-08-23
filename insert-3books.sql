45370b5e2a3406e50e22e4aba6972b5f1a56b0b5
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-08-23 15:02:18 +0200

    inserte 3 books for each author

---
+++ insert-3books.sql
@@ -0,0 +1,31 @@
+INSERT INTO  books(name,category,cover,author) VALUES('Angels & Demons','Adventures','angels&Demons.jpg','Dan Brown');
+ INSERT INTO  books(name,category,cover,author) VALUES('The Da Vinci Code','Mystery','TheDaVinciCode.jpg','Dan Brown');
+ INSERT INTO  books(name,category,cover,author) VALUES('Inferno','Conspiracy','Inferno.jpg','Dan Brown');
+
+ INSERT INTO  books(name,category,cover,author) VALUES('The Mysterious Affair at Styles','Adventures','TheMysteriousAffairAtStyles.jpg','Agatta Christie');
+ INSERT INTO  books(name,category,cover,author) VALUES('The Murder on the Links','Adventures','TheMurderontheLinks.png','Agatta Christie');
+ INSERT INTO  books(name,category,cover,author) VALUES('While the Light Lasts','Adventures','WhileTheLightLasts.jpg','Agatta Christie');
+
+ INSERT INTO  books(name,category,cover,author) VALUES('The Alchemist','Adventures','The Alchemist.jpg','Paulo Coelho');
+ INSERT INTO  books(name,category,cover,author) VALUES('Brida ','Adventures','Brida .jpg','Paulo Coelho');
+ INSERT INTO  books(name,category,cover,author) VALUES('Eleven Minutes','Adventures','ElevenMinutes.jpg','Paulo Coelho');
+
+ INSERT INTO  books(name,category,cover,author) VALUES('The Pillars of the Earth','Mystery','ThePillarsoftheEarth.jpg','Ken Follett');
+ INSERT INTO  books(name,category,cover,author) VALUES('Hornet Fligh','Adventures','HornetFligh.jpg','Ken Follett');
+ INSERT INTO  books(name,category,cover,author) VALUES('A Dangerous Fortunes','Conspiracy','A DangerousFortune.jpg','Ken Follett');
+
+ INSERT INTO  books(name,category,cover,author) VALUES('A Time to Kill','Conspiracy','ATimeToKill.jpg','John Grisham');
+ INSERT INTO  books(name,category,cover,author) VALUES('The Litigators','Adventures','TheLitigators.jpg','John Grisham');
+ INSERT INTO  books(name,category,cover,author) VALUES('Sycamore Row','Mystery','SycamoreRow.jpg','John Grisham');
+
+ INSERT INTO  books(name,category,cover,author) VALUES('Needful Things','Adventures','NeedfulThings.jpg','stephen King');
+ INSERT INTO  books(name,category,cover,author) VALUES('The Long Walk','Conspiracy','TheLongWalk.jpg','stephen King');
+ INSERT INTO  books(name,category,cover,author) VALUES('Misery','Mystery','Misery.jpg','stephen King');
+
+ INSERT INTO  books(name,category,cover,author) VALUES('Kafka on the Shore','Adventures','KafkaOntheShore.jpg','Haruki Murakami');
+ INSERT INTO  books(name,category,cover,author) VALUES('The Wind-Up Bird Chronicle','Mystery','TheWind-UpBirdChronicle.png','Haruki Murakami');
+ INSERT INTO  books(name,category,cover,author) VALUES('Norwegian Wood','Conspiracy','NorwegianWood.jpg','Haruki Murakami');
+
+ INSERT INTO  books(name,category,cover,author) VALUES('NYPDRed','Adventures','NYPDRed.jpg','James Patterson');
+ INSERT INTO  books(name,category,cover,author) VALUES('NeverNever','Mystery','NeverNever.jpg','James Patterson');
+ INSERT INTO  books(name,category,cover,author) VALUES('Along Came A Spider','Conspiracy','AlongCameASpider.jpg','James Patterson');
